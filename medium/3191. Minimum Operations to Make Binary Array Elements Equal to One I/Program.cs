7c1944acb73832043eb4e46be396fc51103f75d9
Emil <emo.vladinov@gmail.com>
2025-03-19 12:20:23 +0100

    daily leetcode

---
+++ medium/3191. Minimum Operations to Make Binary Array Elements Equal to One I/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public int MinOperations(int[] nums)
+    {
+        int minOperations = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                continue;
+            }
+
+            if (nums[i] == 0 && (i + 2) < nums.Length)
+            {
+                nums[i] = 1;
+                nums[i + 1] = nums[i + 1] == 0 ? 1 : 0;
+                nums[i + 2] = nums[i + 2] == 0 ? 1 : 0;
+
+                minOperations++;
+
+            }
+        }
+
+        foreach (int num in nums)
+        {
+            if (num == 0) return -1;
+        }
+
+        return minOperations;
+    }
+}
