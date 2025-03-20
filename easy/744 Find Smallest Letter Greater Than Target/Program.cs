dc3b9def91d9ded407b88158cf890ca07c7d6580
Emil <emo.vladinov@gmail.com>
2025-03-20 18:12:06 +0100

    daily leetcode session, started a binary search playlist

---
+++ easy/744 Find Smallest Letter Greater Than Target/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        int left = 0;
+        int right = letters.Length - 1;
+        int result = -1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (letters[mid] > target)
+            {
+                result = mid;
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return result == -1 ? letters[0] : letters[result];
+    }
+}
