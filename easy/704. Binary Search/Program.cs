eabd03524e8bec39671d9106eaca032c250ce99a
Emil <emo.vladinov@gmail.com>
2025-03-20 01:40:28 +0100

    704. Binary Search

---
+++ easy/704. Binary Search/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int right = nums.Length - 1;
+        int left = 0;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+
+            if (target < nums[mid])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
