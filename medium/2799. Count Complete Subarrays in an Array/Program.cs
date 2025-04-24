15fa10def1db62f0f191556515645fe255d14897
Emil <emo.vladinov@gmail.com>
2025-04-24 21:58:57 +0200

    daily leetcode

---
+++ medium/2799. Count Complete Subarrays in an Array/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public int CountCompleteSubarrays(int[] nums)
+    {
+        int totalUniqueValues = new HashSet<int>(nums).Count;
+        int result = 0;
+
+        Dictionary<int, int> freq = new();
+        int left = 0, distinctCount = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            if (!freq.ContainsKey(nums[right]))
+            {
+                freq[nums[right]] = 0;
+                distinctCount++;
+            }
+            freq[nums[right]]++;
+
+            while (distinctCount == totalUniqueValues)
+            {
+                result += nums.Length - right;
+
+                freq[nums[left]]--;
+                if (freq[nums[left]] == 0)
+                {
+                    freq.Remove(nums[left]);
+                    distinctCount--;
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
